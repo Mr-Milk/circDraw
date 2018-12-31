210b1719a65d9c3370d9220dbc5d3e02451abd96
Tianqin Li <jacklitianqin@gmail.com>
2018-12-31 15:47:44 +0800

    junk

--- cirDraw/static/js/linechart.js
+++ cirDraw/static/js/linechart.js
@@ -1,6 +1,6 @@
 var url = $(location).attr('href').split("/")
 var caseid = url[url.length -1]
-$.getJSON('lenChart_URL', {'caseid': caseid})
+$.getJSON('lenChart_URL', {'case_id': caseid})
 .done(function(lenChartData){
     var x = lenChartData.x
     var y = lenChartData.y
@@ -50,7 +50,7 @@ $.getJSON('lenChart_URL', {'caseid': caseid})
     lenChart.setOption(option1);
 });
 
-$.getJSON('exonChart_URL', {'caseid': caseid})
+$.getJSON('exonChart_URL', {'case_id': caseid})
 .done(function(exonChartData){
     var x = exonChartData.x
     var y = exonChartData.y
@@ -100,7 +100,7 @@ $.getJSON('exonChart_URL', {'caseid': caseid})
     exonChart.setOption(option2);
 });
 
-$.getJSON('isoChart_URL', {'caseid': caseid})
+$.getJSON('isoChart_URL', {'case_id': caseid})
 .done(function(isoChartData){
     var x = isoChartData.x
     var y = isoChartData.y
