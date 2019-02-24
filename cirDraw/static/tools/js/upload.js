b52e90dac3f55e3655be73246eb1d75ee752f5ab
Mr-Milk <zhengym28@mail2.sysu.edu.cn>
2019-02-24 20:46:32 +0800

    fix dead loop in upload.js

--- cirDraw/static/tools/js/upload.js
+++ cirDraw/static/tools/js/upload.js
@@ -24,19 +24,32 @@ $(document).ready(function(){
     })
     var reportID
     $('#submit').click(function(){
+        parameters = {"FileType": $("#software").val(), "Species": $("#species").val(), "expvalue": $("#expvalue").val(), "denvalue": $("#denvalue").val()}
+        parametersString = JSON.stringfy(parameters)
         $.ajax({
             url: '/upload',
             type: 'POST',
             cache: false,
-            data: new FormData($('#fileform')[0]),
+            dataType: "json",
+            data: parametersString,
             processData: false,
             contentType: false
-        }).done(
-            function(reportID) {$('#submitstatus').text('✓ Submitted');
-                reportID = reportID[0]
-        }).fail(
-            function() {$('#processtip').text('Failed to upload, please check your connection and refreash.').css("color", "#CB4042")});
-    
+        })
+        .done(
+            $.ajax({
+                url: '/upload',
+                type: 'POST',
+                cache: false,
+                data: new FormData($('#fileform')[0]),
+                processData: false,
+                contentType: false
+            }).done(
+                function(reportID) {$('#submitstatus').text('✓ Submitted');
+                    reportID = reportID[0]
+            }).fail(
+                function() {$('#processtip').text('Failed to upload, please check your connection and refreash.').css("color", "#CB4042")})
+        )
+
         $('#aftersubmit').show()
         $('#resultbutton').show()
         var sec = 0;
@@ -44,16 +57,18 @@ $(document).ready(function(){
             function(){
                 $('#processtip').text('Processing time: '+ sec + 's')
                 sec++;}, 1000);
-        
-        while (true) {
-            intervalID_2 = setInterval($.getJSON("statusfile").done(function(processState){
-                if (processState == true) {
-                    clearInterval(intervalID_1)
-                    $('#processtip').text('Processing Completed! Your report ID is ' + '<b><i>' + reportID + '</i></b>')
-                    $('#resultbutton').removeAttr("disabled").attr("href", "www.circdraw.com/tools/"+ reportID)
-                    clearInterval(intervalID_2)
-                }
-            }), 5000)
-        }
+
+        $.getJSON("statusfile").done(function(processState){
+            if (processState == true) {
+                clearInterval(intervalID_1)
+                $('#processtip').text('Processing Completed! Your report ID is ' + '<b><i>' + reportID + '</i></b>')
+                $('#resultbutton').removeAttr("disabled").attr("href", "www.circdraw.com/tools/"+ reportID)
+            }
+
+            if (processState == false) {
+                clearInterval(intervalID_1)
+                $('#processtip').text('Processing Failed!')
+            }
+        })
     })
 });
