2b31fb9390714742a21ac061ca22faf9c44a2b19
Tianqin Li <jacklitianqin@gmail.com>
2019-03-22 20:34:58 +0800

    modify operating logic of raw data; add oneline.py for simply reproduction of database manipulate

---
+++ related_genome_info/oneline.py
@@ -0,0 +1,56 @@
+import os
+
+
+def main():
+    # add annotation
+    m1a_annotation_db()
+    m6a_annotation_db()
+    m5c_annotation_db()
+    snp_annotation_db()
+
+
+    # aggregate snp to each
+
+
+# ================== m1a ============================
+def m1a_annotation_db():
+    command_annotation = """python3 annotation.py RMBase_hg19_all_m1A_site.txt m1a"""
+    command_db = """python3 mysql_db.py admin_login.json -annotation RMBase_hg19_all_m1A_site.json RMBase_hg19_all_m1A_site_length_.json table_column.txt tools_m1a"""
+    current_path = os.getcwd()
+    os.chdir("raw_data/m1A/")
+    os.system(command_annotation)
+    os.system(command_db)
+    os.chdir(current_path)
+
+
+def m6a_annotation_db():
+    command_annotation = """python3 annotation.py RMBase_hg19_all_m6A_site.txt m6a"""
+    command_db = """python3 mysql_db.py admin_login.json -annotation RMBase_hg19_all_m6A_site.json RMBase_hg19_all_m6A_site_length_.json table_column.txt tools_m6a"""
+    current_path = os.getcwd()
+    os.chdir("raw_data/m6A/")
+    os.system(command_annotation)
+    os.system(command_db)
+    os.chdir(current_path)
+
+
+def m5c_annotation_db():
+    command_annotation = """python3 annotation.py RMBase_hg19_all_m5C_site.txt m5c"""
+    command_db = """python3 mysql_db.py admin_login.json -annotation RMBase_hg19_all_m5C_site.json RMBase_hg19_all_m5C_site_length_.json table_column.txt tools_m5c"""
+    current_path = os.getcwd()
+    os.chdir("raw_data/m5c/")
+    os.system(command_annotation)
+    os.system(command_db)
+    os.chdir(current_path)
+
+def snp_annotation_db():
+    command_annotation = """python3 annotation.py RMBase_hg19_all_mod_snp_disease.txt snp"""
+    command_db = """python3 mysql_db.py admin_login.json -annotation RMBase_hg19_all_mod_snp_disease.json RMBase_hg19_all_mod_snp_disease_length_.json table_column.txt tools_snp"""
+    current_path = os.getcwd()
+    os.chdir("raw_data/SNP/")
+    os.system(command_annotation)
+    os.system(command_db)
+    os.chdir(current_path)
+
+
+if __name__ == '__main__':
+    main()
--- related_genome_info/process_pkg/mysql_db.py
+++ related_genome_info/process_pkg/mysql_db.py
@@ -3,6 +3,7 @@ import ijson
 import json
 from annotation import readfile, string_or_int
 import random
+import sys
 
 
 # load json data:
@@ -145,6 +146,7 @@ def column_is_valid_core(table_columns, line):
             line[name]
         return True
     except:
+        print("Table_columns:", table_columns)
         return False
 
 
@@ -349,7 +351,7 @@ def add_one_column_update(cnx, cursor, table_name, column, value_pairs):
     name = column.split(' ')[0]
     update_o = """UPDATE `""" + str(table_name)  + """` SET """ + str(name) + """ = """
     for i in value_pairs:
-        update = update_o + str(i[1]) + """ WHERE chr_ci = """ + str(i[0]) + """;"""
+        update = update_o + str(i['length']) + """ WHERE chr_ci = """ + str(i['name']) + """;"""
         cursor.execute(update)
     cnx.commit()
 
@@ -453,65 +455,115 @@ def commit_db(cnx, cursor):
     cursor.close()
     cnx.close()
 
-def main():
-    login_file_name = 'admin_login.json'
-    annotation = True
-    add = True
-    scale = True
-    gene_wiki = True
+
+def get_input_value(sys_input):
+    """Sys_input is exact the same as sys.argv, return dict contain parameters and lists of values of that parameter. Default is stored by {'default': [default_1, default_2...]}"""
+    args = {}
+    parameter_now = None
+    obligated = True
+    default = []
+    sys_input = sys_input[1:]
+    for i in range(len(sys_input)):
+
+        if sys_input[i][0] == '-':
+            obligated = False
+            parameter_now = sys_input[i][1:]
+            # error when parameter_now is 'default'
+            if parameter_now == 'default':
+                raise ValueError("You have '-default' in your command line input, please include them without '-default'")
+            args[parameter_now] = []
+        elif obligated:
+            print("default")
+            default.append(sys_input[i])
+        else:
+            args[parameter_now].append(sys_input[i])
+    re = dict({'default': default}, **args)
+    return re
+
+
+def get_parameter_or_None(dic, para_name):
+    if para_name in dic.keys():
+        return dic[para_name]
+    else:
+        return None
+
+
+
+
+
+
+def main(sys_argv):
+    paras = get_input_value(sys_argv)
+    default = paras['default']
+    assert len(default) >= 1, "At least database login info is required!"
+    login_file_name = default[0]
+    print(paras)
+    annotation = get_parameter_or_None(paras, 'annotation')
+    print(annotation)
+    add = get_parameter_or_None(paras, 'add')
+    scale = get_parameter_or_None(paras, 'scale')
+    gene_wiki = get_parameter_or_None(paras, 'gene_wiki')
 
     # initial connection
     cnx, cursor = connect_to_db(login_file_name)
 
 
     # insert
-    # data_file_name_annotation = 'test.json'
-    data_file_name_annotation = 'gencode_annotation.json'
-    length_info_name_annotation = 'gencode_length_annotation.json'
-    table_columns_annotation = ("gene_type VARCHAR(50) NOT NULL","gene_name VARCHAR(200) NOT NULL","chr_ci VARCHAR(50) NOT NULL", "gene_start INT NOT NULL", "gene_end INT NOT NULL", "gene_id VARCHAR(200) NOT NULL")
-    table_name_annotation = "tools_annotation"
+    # ====================
     if annotation:
+        """
+        >>> annotation = ['gencode_annotation.json', 'gencode_length_annotation.json', 'table_columns_info_file', 'tools_annotation']
+        >>> table_columns_annotation = ("gene_type VARCHAR(50) NOT NULL","gene_name VARCHAR(200) NOT NULL","chr_ci VARCHAR(50) NOT NULL", "gene_start INT NOT NULL", "gene_end INT NOT NULL", "gene_id VARCHAR(200) NOT NULL")
+        """
+        data_file_name_annotation = annotation[0]# 'gencode_annotation.json'
+        length_info_name_annotation = annotation[1]
+        table_columns_info_file = annotation[2]
+        # table_columns_annotation
+        table_columns_annotation = []
+        with open(table_columns_info_file) as table_cfile:
+            lines = table_cfile.readlines()
+            for cline in lines:
+                table_columns_annotation.append(cline[:-1])
+        print("Table_input:",table_columns_annotation)
+        table_columns_annotation = tuple(table_columns_annotation)
+
+        # table_db_name
+        table_name_annotation = annotation[3]
         load_and_insert(cnx, cursor, data_file_name_annotation, table_columns_annotation, table_name_annotation, length_info_name_annotation)
         cnx.commit()
+
 
     # add species info
     if add:
         add_column(cnx, cursor, table_name_annotation, ["species VARCHAR(300) NOT NULL"], ["'human'"])
         cnx.commit()
     # create scale(min start, max end) table
-    table_name_scale = "tools_scalegenome"
-    table_columns_scale = ("species VARCHAR(300) NOT NULL", "chr_ci VARCHAR(30) NOT NULL", "gene_min_start INT NOT NULL", "gene_max_end INT NOT NULL")
     if scale:
-        toos_scale(cnx, cursor, table_name_scale, table_name_annotation, table_columns_scale, "chr_ci", ["gene_end", "gene_max_end"], ["gene_start", "gene_min_start"])
+        """scale = ["tools_scalegenome", "table_columns_scale_column_file_name", "group_by_column_name", max_by, min_by, name_max, name_min, species]
+        table_columns_scale = ("species VARCHAR(300) NOT NULL", "chr_ci VARCHAR(30) NOT NULL", "gene_min_start INT NOT NULL", "gene_max_end INT NOT NULL")
+        """
+        table_name_scale = scale[0]
+        table_columns_scale_column_file_name = scale[1]
+        table_columns_scale = []
+        with open(table_columns_scale_column_file_name) as mm:
+            lines = mm.readline()
+            for mma in lines:
+                table_columns_scale.append(mma)
+        table_columns_scale = tuple(table_columns_scale)
+
+
+        group_by = scale[2]
+        max_column = [scale[3], scale[5]]
+        min_column = [scale[4], scale[6]]
+        species = scale[7].__repr__()
+        toos_scale(cnx, cursor, table_name_scale,table_name_annotation, table_columns_scale, group_by, max_column, min_column, species = species)
         cnx.commit()
     if gene_wiki:
-        value_pairs = [
-            ["'chr1'",  249250621],
-            ["'chr2'",  243199373],
-            ["'chr3'", 198022430],
-            ["'chr4'",  191154276],
-            ["'chr5'", 180915260],
-            ["'chr6'", 171115067],
-            ["'chr7'", 159138663],
-            ["'chr8'", 146364022],
-            ["'chr9'", 141213431],
-            ["'chr10'", 135534747],
-            ["'chr11'", 135006516],
-            ["'chr12'", 133851895],
-            ["'chr13'", 115169878],
-            ["'chr14'", 107349540],
-            ["'chr15'", 102531392],
-            ["'chr16'",  90354753],
-            ["'chr17'", 81195210],
-            ["'chr18'", 78077248],
-            ["'chr19'", 59128983],
-            ["'chr20'", 63025520],
-            ["'chr21'", 48129895],
-            ["'chr22'", 51304566],
-            ["'chrX'", 155270560],
-            ["'chrY'", 59373566],
-            ["'chrM'", 16023]
-        ]
+        """gene_wiki = [wiki_len_file, table_name_scale]"""
+        wiki_len_file = gene_wiki[0]
+        table_name_scale = gene_wiki[1]
+        with open(wiki_len_file) as wiki_file:
+            value_pairs = json.loads(wiki_file.read())
         add_one_column_update(cnx, cursor, table_name_scale, "genelens_wiki INT", value_pairs)
         add_one_column(cnx, cursor, table_name_scale, "id INT")
 
@@ -519,5 +571,13 @@ def main():
     commit_db(cnx, cursor)
 
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
-   main()
+   main(sys.argv)
+
